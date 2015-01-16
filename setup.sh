3ec798f6077b02ed5ba56924fcd38c0d9e1aeda9
Spencer Wood <woodsp@users.noreply.github.com>
2015-01-16 15:21:08 -0800

    US keyboard settings now happen during install; no longer need to revise config

--- setup.sh
+++ setup.sh
@@ -6,5 +6,4 @@ yes | apt-get install python-picamera gpsd gpsd-clients python-gps emacs ntpd
 crontab -l | { cat; echo "@reboot /usr/bin/python /home/pi/pyskyview/test_camera.py > /home/pi/pyskyview/log 2>&1"; } | crontab -
 crontab -l | { cat; echo "*/1 * * * * /home/pi/pyskyview/shoutout.sh > /home/pi/pyskyview/shoutoutlog 2>&1"; } | crontab -
 cp config_files/gpsd /etc/default
-cp config_files/keyboard /etc/default
 cp config_files/ntp.conf /etc/
