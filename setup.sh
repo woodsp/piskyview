c9c7da60880c3819782930649ceebf2816457d4d
Spencer Wood <woodsp@users.noreply.github.com>
2015-01-16 15:45:43 -0800

    don't need ntpd (it gets installed by default).  apt upgrade happens after first boot now (see wiki).

--- setup.sh
+++ setup.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-yes | apt-get update
-yes | apt-get upgrade
-yes | apt-get install python-picamera gpsd gpsd-clients python-gps emacs ntpd
+yes | apt-get install python-picamera gpsd gpsd-clients python-gps emacs
 crontab -l | { cat; echo "@reboot /usr/bin/python /home/pi/pyskyview/test_camera.py > /home/pi/pyskyview/log 2>&1"; } | crontab -
 crontab -l | { cat; echo "*/1 * * * * /home/pi/pyskyview/shoutout.sh > /home/pi/pyskyview/shoutoutlog 2>&1"; } | crontab -
 cp config_files/gpsd /etc/default
