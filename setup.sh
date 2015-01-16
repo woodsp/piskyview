2810be931e5603193a2a10865735a45a96e95068
Richard Sharp <richpsharp@gmail.com>
2015-01-16 13:25:34 -0800

    pointing files to the config directory

--- setup.sh
+++ setup.sh
@@ -5,6 +5,6 @@ apt-get upgrade
 apt-get install python-picamera gpsd gpsd-clients python-gps emacs ntpd
 crontab -l | { cat; echo "@reboot /usr/bin/python /home/pi/pyskyview/test_camera.py > /home/pi/pyskyview/log 2>&1"; } | crontab -
 
-cp gpsd /etc/default
-cp keyboard /default/keyboard
-cp ntp.conf /etc/
+cp config_files/gpsd /etc/default
+cp config_files/keyboard /default/keyboard
+cp config_files/ntp.conf /etc/
