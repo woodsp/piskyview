881244b8115ab70ee855a0d2897576fda4db615f
Richard Sharp <richpsharp@gmail.com>
2015-01-16 14:17:23 -0800

    piping yes to the apt-get script and copying keyboard to the default

--- setup.sh
+++ setup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-apt-get update
-apt-get upgrade
-apt-get install python-picamera gpsd gpsd-clients python-gps emacs ntpd
+yes | apt-get update
+yes | apt-get upgrade
+yes | apt-get install python-picamera gpsd gpsd-clients python-gps emacs ntpd
 crontab -l | { cat; echo "@reboot /usr/bin/python /home/pi/pyskyview/test_camera.py > /home/pi/pyskyview/log 2>&1"; } | crontab -
-
+crontab -l | { cat; echo "*/1 * * * * /home/pi/pyskyview/shoutout.sh > /home/pi/pyskyview/shoutoutlog 2>&1"; } | crontab -
 cp config_files/gpsd /etc/default
-cp config_files/keyboard /default/keyboard
+cp config_files/keyboard /etc/default
 cp config_files/ntp.conf /etc/
